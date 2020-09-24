4b807886dfcacb32a05a21b9f69076ac8e7b1467
D4RK PH1N1X <71398334+D4RK-PH1N1X@users.noreply.github.com>
2020-09-24 22:00:47 +0600

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,27 @@
+echo "Installing..."
+sleep 2
+unzip FB.zip
+mkdir FB
+mv fb.py FB
+
+mv install.txt FB
+apt update
+apt upgrade
+apt install git
+apt install python
+apt install python2
+apt install wget
+apt install lolcat
+apt install vim
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
+pip install --upgrade pip
+pip2 install --upgrade pip
+pip install lolcat
+pip install bs4
+pip2 install bs4
+pip install colorama
+chmod +x *
+echo "Installing finished...Now run it by python3 D4RKPBF.py"
